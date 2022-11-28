8683ecf283b205492b69d60e67c7a954bc116971
parkergeorge13 <parkergeorge13@gmail.com>
2022-11-28 16:33:11 -0700

    added superuser to admin.py

--- kidney_app/admin.py
+++ kidney_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account, Journal_Entry, Food, Nutrient
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Journal_Entry)
+admin.site.register(Food)
+admin.site.register(Nutrient)
