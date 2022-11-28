e3b2298753b422cd9884e1c49b0355802e83d552
parkergeorge13 <parkergeorge13@gmail.com>
2022-11-28 16:19:56 -0700

    Perfect database integration

--- kidney_app/models.py
+++ kidney_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    gender = models.CharField(max_length=25)
+    birth_date = models.DateField(default=datetime.today, blank=True)
+    height = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+    condition = models.CharField(max_length=25)
+
+    def __str__(self):
+        return (self.full_name)
+
+    @property
+    def full_name(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    def save(self):
+        self.first_name = self.first_name.upper()
+        self.last_name = self.last_name.upper()
+        super(Account, self).save()
+
+class Journal_Entry(models.Model):
+    date = models.DateField(default=datetime.today, blank=True)
+    meal_category = models.CharField(max_length=25)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.food_desc)
+
+class Food(models.Model):
+    food_desc = models.CharField(max_length=25)
+    journal_entry = models.ManyToManyField(Journal_Entry, blank=True)
+    def __str__(self):
+        return (self.food_desc)
+
+class Nutrient(models.Model):
+    sodium = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0) 
+    water = models.IntegerField(default=0)
+    potassium = models.IntegerField(default=0)
+    phosphorus = models.IntegerField(default=0)
+    food = models.ManyToManyField(Food, blank=True)
+
+    def __str__(self):
+        return (self.food)
