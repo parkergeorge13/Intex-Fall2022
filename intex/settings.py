bbdd32c27ed611439034efbeb6fecb2314e430fa
parkergeorge13 <parkergeorge13@gmail.com>
2022-11-29 18:18:46 -0700

    added the api configuration

--- intex/settings.py
+++ intex/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'intex',
         'USER': 'postgres',
-        'PASSWORD': 'faline',
+        'PASSWORD': 'admin',
         'HOST': 'localhost'
     }
 }
